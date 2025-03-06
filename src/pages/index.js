39419e777a567203d7207f8d7a5eaa22bab7ff13
jxmedia <josh@thejxmediagroup.com>
2025-03-05 20:00:28 -0500

    color update

--- src/pages/index.js
+++ src/pages/index.js
@@ -174,7 +174,7 @@ const IndexPage = () => {
           </div>
         </div>
 
-        <div class="bg-gray-50 py-24 sm:py-32">
+        <div class="bg-sky-200 py-24 sm:py-32">
           <div class="mx-auto max-w-7xl px-6 lg:px-8">
             <div class="mx-auto grid max-w-2xl grid-cols-1 items-start gap-x-8 gap-y-16 sm:gap-y-24 lg:mx-0 lg:max-w-none lg:grid-cols-2">
               <div class="lg:pr-4">
